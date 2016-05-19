93835e4071c1aee57dc438fdaf171f6ef05654d0
Jacobocg <jacobcg@gmail.com>
2016-05-19 19:20:02 +0200

    Update Prueba.java

--- Prueba.java
+++ Prueba.java
@@ -1,1 +1,7 @@
-public class Prueba{public static void main(String[] args){System.out.println("Hola");}}
+public class Prueba{
+  public static void main(String[] args){
+    System.out.println("Esto es un programa en java");
+    for(int i = 0; i < 10; i++)
+      System.out.println("Comprobando ejecución " + i);
+  }
+}
